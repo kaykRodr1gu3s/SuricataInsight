2d07d160c547bdb272c985dd7b838c600281e255
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2024-01-22 15:43:59 -0300

    a

--- Main.py
+++ Main.py
@@ -7,39 +7,44 @@ import os
 
 
 class data_visualization:
-    def __init__(self) -> None:
-        self.cvs_file = directory_helper.directory(os.listdir())
+    def __init__(self):
+        self.cvs_file = directory_helper.directory(os.listdir()).dir_content
 
     def open_csv(self) -> pd.DataFrame:
         '''
         This function, will open the Suricata_datas.csv and transform in a pd.core.frame.DataFrame object
         '''
+        list_df = []
+        
+        for csv in self.cvs_file:
+            df = pd.read_csv(csv)
+            list_df.append(df)
+        return list_df
 
-        return pd.read_csv('Suricata_Datas.csv')
-
-
-    def transform_data(self, pd_data) -> pd.DataFrame:
+    def transform_data(self, pd_data: list) -> pd.DataFrame:
         '''
         The transform_data function will rename the column, and will parse the Time column for a datetime format.
         '''
+        df_data = []
+        for csv in pd_data:
+            df = csv
+            df_columns_name = df.columns.to_list()
 
-        df = pd_data
-        df_columns_name = df.columns.to_list()
+            df.rename(columns={
+                df_columns_name[0]: 'Time', df_columns_name[1]: 'IP Source', df_columns_name[2]: 'Port Source',
+                                df_columns_name[3]: 'Sorce Port', df_columns_name[3]: 'IP Destination',df_columns_name[4]: 'Destination Port', 
+                                df_columns_name[5] :'Event Type', df_columns_name[6] : 'Severity', df_columns_name[7]: 'Alert Signature'
+                                }, inplace=True)
 
-        df.rename(columns={
-            df_columns_name[0]: 'Time', df_columns_name[1]: 'IP Source', df_columns_name[2]: 'Port Source',
-                            df_columns_name[3]: 'Sorce Port', df_columns_name[3]: 'IP Destination',df_columns_name[4]: 'Destination Port', 
-                            df_columns_name[5] :'Event Type', df_columns_name[6] : 'Severity', df_columns_name[7]: 'Alert Signature'
-                            }, inplace=True)
+            df.loc[-1] = df_columns_name
+            df.index = df.index + 1
+            df = df.sort_index()
 
-        df.loc[-1] = df_columns_name
-        df.index = df.index + 1
-        df = df.sort_index()
+            df['Time'] = df['Time'].apply(lambda time: time.split('.')[0])
+            df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m-%dT%H:%M:%S')
+            df_data.append(df)
 
-        df['Time'] = df['Time'].apply(lambda time: time.split('.')[0])
-        df['Time'] = pd.to_datetime(df['Time'], format='%Y-%m-%dT%H:%M:%S')
-
-        return df
+        return df_data
 
 
     def counter_function(self, data: pd.DataFrame) -> list:
@@ -47,8 +52,7 @@ class data_visualization:
         This function will counter all the Alert Signature and will save in a list
         '''
         counter_Alert_Signature = Counter()
-
-        counter_Alert_Signature
+        
         counter_Alert_Signature.update(data['Alert Signature'])
         counter_Alert_Signature = counter_Alert_Signature.most_common()
 
@@ -66,29 +70,29 @@ class data_visualization:
         return all_data 
 
 
-    def plotting_datas(self, datas_for_plot : list):
-        '''
-        This function will plot all datas that are available, and save with the name Suricata_Datas.png
-        '''
+    # def plotting_datas(self, datas_for_plot : list):
+    #     '''
+    #     This function will plot all datas that are available, and save with the name Suricata_Datas.png
+    #     '''
 
-        plt.barh(datas_for_plot[0],datas_for_plot[1])
-        plt.title('Graphic of alert')
-        plt.xlabel('Alert Names')
-        plt.ylabel('Quantity')
-        plt.xticks(rotation=45, ha='right')
-        plt.subplots_adjust(left=0.4)
-        plt.grid(True)
-        plt.tight_layout()
-        plt.savefig('Graphic.png', bbox_inches='tight')
-        plt.show()
+    #     plt.barh(datas_for_plot[0],datas_for_plot[1])
+    #     plt.title('Graphic of alert')
+    #     plt.xlabel('Alert Names')
+    #     plt.ylabel('Quantity')
+    #     plt.xticks(rotation=45, ha='right')
+    #     plt.subplots_adjust(left=0.4)
+    #     plt.grid(True)
+    #     plt.tight_layout()
+    #     plt.savefig('Graphic.png', bbox_inches='tight')
+    #     plt.show()
         
-    def main(self):
-        csv = self.open_csv()
-        trans_data = self.transform_data(csv)
-        counted_data = self.counter_function(trans_data)
-        self.plotting_datas(counted_data)
+    # def main(self):
+    #     csv = self.open_csv()
+    #     trans_data = self.transform_data(csv)
+    #     counted_data = self.counter_function(trans_data)
+    #     self.plotting_datas(counted_data)
 
 
 
 a = data_visualization()
-a.main()
+a.transform_data(a.open_csv())
